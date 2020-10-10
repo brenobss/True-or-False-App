01c8275743392fb384ee11d7a7b1fb9e0f62a3a2
Breno Barreto <brenobarreto10@gmail.com>
2020-10-09 23:02:34 -0300

    Faltam verificações, quase pronto

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,68 @@
+import {modalTrue} from './modal'
+import {modalFalse} from './modal'
 
 document.getElementById("submit").onclick = verification
+
+
 
 function verification() {
     let operator1 = document.getElementById("operador1").value
     let conditional = document.getElementById("condicional").value
     let operator2 = document.getElementById("operador2").value
 
+
+    console.log(operator1)
+    console.log(conditional)
+    console.log(operator2)
+
     switch(conditional){
-        case "=":
-            if (operator1 == operator2)
-                
-              
+        case "===":
+            if (operator1 === operator2)
+                modalTrue()
             else
-                alert("FALSE")
+                modalFalse()
+        break
+        case "!==":
+            if (operator1 !== operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case "==":
+            if (operator1 == operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case "!=":
+            if (operator1 != operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case ">=":
+            if (operator1 >= operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case "<=":
+            if (operator1 <= operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case ">":
+            if (operator1 > operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
+        case "<":
+            if (operator1 < operator2)
+                modalTrue()
+            else
+                modalFalse()
+        break
     }
 }
---
+++ src/js/modal.js
@@ -0,0 +1,9 @@
+export function modalTrue(){
+    document.getElementById("exampleModalLabel").innerHTML = "Resultado"
+    document.getElementById("modalBody").innerHTML = "TRUE"
+    }
+
+export function modalFalse(){
+    document.getElementById("exampleModalLabel").innerHTML = "Resultado"
+    document.getElementById("modalBody").innerHTML = "FALSE"
+}
