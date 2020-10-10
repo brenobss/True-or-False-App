a9a69526dda665764833619113e3cbb54385f0e1
Breno Barreto <brenobarreto10@gmail.com>
2020-10-10 12:21:57 -0300

    problemas com o operador '==='

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 import {modalTrue} from './modal'
 import {modalFalse} from './modal'
+import {transformerString} from './modal'
 
 document.getElementById("submit").onclick = verification
 
 
 
 function verification() {
-    let operator1 = document.getElementById("operador1").value
+    let operator1 = transformerString(document.getElementById("operador1").value)
     let conditional = document.getElementById("condicional").value
-    let operator2 = document.getElementById("operador2").value
+    let operator2 = transformerString(document.getElementById("operador2").value)
 
 
     console.log(operator1)
@@ -41,25 +42,25 @@ function verification() {
                 modalFalse()
         break
         case ">=":
-            if (operator1 >= operator2)
+            if (parseFloat(operator1) >= parseFloat(operator2))
                 modalTrue()
             else
                 modalFalse()
         break
         case "<=":
-            if (operator1 <= operator2)
+            if (parseFloat(operator1) <= parseFloat(operator2))
                 modalTrue()
             else
                 modalFalse()
         break
         case ">":
-            if (operator1 > operator2)
+            if (parseFloat(operator1) > parseFloat(operator2))
                 modalTrue()
             else
                 modalFalse()
         break
         case "<":
-            if (operator1 < operator2)
+            if (parseFloat(operator1) < parseFloat(operator2))
                 modalTrue()
             else
                 modalFalse()
--- src/js/modal.js
+++ src/js/modal.js
@@ -7,3 +7,14 @@ export function modalFalse(){
     document.getElementById("exampleModalLabel").innerHTML = "Resultado"
     document.getElementById("modalBody").innerHTML = "FALSE"
 }
+
+export function transformerString(operator){
+    if (operator.includes("'") || operator.includes("\"")){
+        operator = operator.replaceAll("'", "")
+        operator = operator.replaceAll("\"", "")
+        operator = operator.toString()
+        console.log("Esse ta com String", operator)
+        return operator.toString()
+    }
+    else return operator
+}
